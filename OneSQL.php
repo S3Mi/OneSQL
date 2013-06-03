c69a39ad5330a561079c1042a2cd46235fc37ba3
Kamil Ćwikowski <kcwikowski@gmail.com>
2013-06-03 22:41:23 +0100

    The beginings

---
+++ OneSQL.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Kamil Ćwikowski
+ * Date: 03.06.13
+ * Time: 20:46
+ * To change this template use File | Settings | File Templates.
+ */
+namespace OneSQL;
+
+class OneSQL {
+
+}
